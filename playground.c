3e2fcab53a58c32144190f5e945dead7d47172bf
edd255 <abaddon@disroot.org>
2023-01-22 21:43:46 +0100

    tests: added some proper tests

--- playground.c
+++ playground.c
@@ -1,26 +1,61 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <assert.h>
 #include "src/str.h"
 
-int main()
+
+void test_length_1()
+{
+        str_t* string = string_init("edd255");
+        assert(string -> length == 6);
+        string_free(string);
+}
+
+void test_equality_1()
 {
         str_t* string1 = string_init("edd255");
         str_t* string2 = string_init("edd255");
-        printf("%s\n", string1 -> data);
-        if (string1 -> length == 6) {
-                printf("Has length 6.\n");
-        }
-        if (string_equals(string1, string2)) {
-                printf("Strings are equal.\n");
-        }
-        if (string_is_substring_of(string1, string2)) {
-                printf("Are substrings.\n");
-        }
-        int idx = string_index_of(string1, "d");
-        printf("Index of d: %d\n", idx);
-        string_fill(string1, "c");
-        printf("New string: %s\n", string1 -> data);
+        assert(string_equals(string1, string2));
         string_free(string1);
         string_free(string2);
+}
+
+void test_substring_1()
+{
+        str_t* string = string_init("edd255");
+        str_t* sub = string_init("d2");
+        assert(string_is_substring_of(string, sub));
+        string_free(string);
+        string_free(sub);
+}
+
+void test_index_of_1()
+{
+        str_t* string = string_init("edd255");
+        assert(string_index_of(string, "d") == 1);
+        string_free(string);
+}
+
+void test_append()
+{
+        str_t* string1 = string_init("Hello ");
+        str_t* string2 = string_init("World!");
+        str_t* string3 = string_append(string1, string2);
+        str_t* string4 = string_init("Hello World!");
+        assert(string3 -> data == string3 -> data);
+        string_free(string1);
+        string_free(string2);
+        string_free(string3);
+        string_free(string4);
+}
+
+
+int main()
+{
+        test_length_1();
+        test_equality_1();
+        test_substring_1();
+        test_index_of_1();
+        test_append();
         return 0;
 }
