d58a139ec3361214b996d85881faa0b19a32d563
edd255 <abaddon@disroot.org>
2023-01-23 08:01:41 +0100

    style(header): increased intendation

--- src/str.h
+++ src/str.h
@@ -1,19 +1,19 @@
 #include <stddef.h>
 
 typedef struct {
-    size_t length;
-    char* data;
+        size_t length;
+        char* data;
 } str_t;
 
 typedef struct {
-    str_t* (* const init)(char* data);
-    void (* const free)(str_t* self);
-    str_t* (* const prepend)(str_t*, str_t*);
-    str_t* (* const append)(str_t*, str_t*);
-    bool (* const equals)(str_t* self, str_t* other);
-    bool (* const is_substring_of)(str_t* self, str_t* other);
-    int (* const index_of)(str_t* self, char* character);
-    void (* const fill)(str_t* self, char* character);
+        str_t* (* const init)(char* data);
+        void (* const free)(str_t* self);
+        str_t* (* const prepend)(str_t*, str_t*);
+        str_t* (* const append)(str_t*, str_t*);
+        bool (* const equals)(str_t* self, str_t* other);
+        bool (* const is_substring_of)(str_t* self, str_t* other);
+        int (* const index_of)(str_t* self, char* character);
+        void (* const fill)(str_t* self, char* character);
 } namespace_string;
 
 extern namespace_string const string;
