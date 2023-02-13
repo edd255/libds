9e4a805d606d426d1174886b64bc96ac500e21e4
edd255 <abaddon@disroot.org>
2023-02-13 21:30:25 +0100

    refactor: include guards, typedef void* to var

--- src/ds.h
+++ src/ds.h
@@ -1,6 +1,9 @@
+#ifndef DS_H
+#define DS_H
+
 #include <stddef.h>
 
-#define var void
+typedef void* var;
 
 //--- STRINGS -----------------------------------------------------------------
 
@@ -8,13 +11,13 @@ typedef struct vtab_string_t vtab_string_t;
 typedef struct str_t str_t;
 
 struct vtab_string_t {
-        var (*free)(str_t* self);
+        void (*free)(str_t* self);
         str_t* (*prepend)(str_t*, str_t*);
         str_t* (*append)(str_t*, str_t*);
         bool (*equals)(str_t* self, str_t* other);
         bool (*is_substring_of)(str_t* self, str_t* other);
         int (*index_of)(str_t* self, char* character);
-        var (*fill)(str_t* self, char* character);
+        void (*fill)(str_t* self, char* character);
 };
 
 struct str_t {
@@ -24,3 +27,5 @@ struct str_t {
 };
 
 str_t* string_init(char* data);
+
+#endif
